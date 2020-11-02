2de2799479b5de92b083540be3013ea1e94b7417
Crowdbotics <team@crowdbotics.com>
2020-11-02 19:16:26 +0000

    Update models

--- backend/donahue_22222/wsgi.py
+++ backend/donahue_22222/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'donahue_22222.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "donahue_22222.settings")
 
 application = get_wsgi_application()
